17ec462e977f6dae3aa7b234c55332a54ff42437
artimmy <arthurtimmy@hotmail.com>
2023-04-16 17:56:11 -0300

    feat: creatre pokemon controller (facade)

--- lib/app/features/pokemon/pokemon_module.dart
+++ lib/app/features/pokemon/pokemon_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_challenge_application/app/features/pokemon/domain/usecases/get_pokemon/get_pokemon_usecase.dart';
+import 'package:flutter_challenge_application/app/features/pokemon/presenter/controllers/pokemon_controller.dart';
 import 'package:flutter_challenge_application/app/features/pokemon/presenter/pages/pokemon_page.dart';
 import 'package:flutter_challenge_application/app/features/pokemon/presenter/stores/pokemon_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -9,13 +10,19 @@ import 'infra/repositories/get_pokemon_repository.dart';
 class PokemonModule extends Module {
   @override
   final List<Bind> binds = [
-    // GetEmailByCpf
+    // Get Pokemon
     Bind.factory((i) => GetPokemonUsecase(i())),
     Bind.factory((i) => GetPokemonRepository(i())),
     Bind.factory((i) => GetPokemonDatasource(httpService: i())),
 
+    // Store
     Bind.lazySingleton(
       (i) => PokemonStore(getPokemonUsecase: i()),
+    ),
+
+    // Controller
+    Bind.lazySingleton(
+      (i) => PokemonController(pokemonStore: i()),
     ),
   ];
 
@@ -23,7 +30,9 @@ class PokemonModule extends Module {
   final List<ModularRoute> routes = [
     ChildRoute(
       '/',
-      child: (context, __) => const PokemonPage(),
+      child: (context, __) => PokemonPage(
+        pokemonController: Modular.get<PokemonController>(),
+      ),
     ),
   ];
 }
