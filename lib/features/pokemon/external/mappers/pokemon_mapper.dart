06b02399fa77fd4f93392659ed415a917fb76edd
artimmy <arthurtimmy@hotmail.com>
2023-04-16 13:31:44 -0300

    refactor: refactor mapper

--- lib/features/pokemon/external/mappers/pokemon_mapper.dart
+++ lib/features/pokemon/external/mappers/pokemon_mapper.dart
@@ -1,18 +1,10 @@
 import 'package:flutter_challenge_application/features/pokemon/domain/entities/pokemon_entity.dart';
 
-class PokemonMapper extends PokemonEntity {
-  const PokemonMapper({
-    required String name,
-    required String picture,
-    required String ability,
-  }) : super(
-          name: name,
-          picture: picture,
-          ability: ability,
-        );
+class PokemonMapper {
+  const PokemonMapper._();
 
-  factory PokemonMapper.fromMap(Map<String, dynamic> map) {
-    return PokemonMapper(
+  static PokemonEntity fromMap(Map<String, dynamic> map) {
+    return PokemonEntity(
       name: map['name'],
       picture: map['sprites']['other']['official-artwork']['front_default'],
       ability: map['abilities']['ability']['name'],
